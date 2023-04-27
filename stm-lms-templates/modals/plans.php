5e7cbcb1e842e951a81ddfc5f0652262ee40bec0
Алексей <6763410@gmail.com>
2023-04-28 02:09:18 +0500

    Plans style changing

--- stm-lms-templates/modals/plans.php
+++ stm-lms-templates/modals/plans.php
@@ -5,7 +5,13 @@
 
     $curriculum = new STM_THEME_CHILD_Curriculum;
     $plans      = $curriculum->plans;
+    $secondary_color = STM_LMS_Options::get_option('secondary_color', '#f2b91e');
 ?>
+<style type="text/css">
+    :root{
+        --plans-color: <?php echo esc_attr( $secondary_color ); ?>
+    }
+</style>
 <div class="modal fade stm-lms-modal-plans" tabindex="-1" role="dialog" aria-labelledby="stm-lms-modal-login">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <a href="#" class="modal-close" data-dismiss="modal"></a>
@@ -14,27 +20,36 @@
                 <div class="pricing">
                     <div class="row">
                         <?php
-                            foreach ($plans as $plan) :
+                            foreach ($plans as $key => $plan) :
                                 $price = STM_THEME_CHILD_Curriculum::plan_price( $course_id, $plan['name'] );
                         ?>
-                            <div class="col-xs-12 col-sm-6 col-md-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <div class="card-content">
-                                            <h5 class="card-title text-muted text-uppercase text-center">
-                                                <?php echo $plan['name']; ?>
-                                            </h5>
-                                            <h6 class="card-price text-center">
-                                                <?php echo STM_LMS_Helpers::display_price( $price ); ?>
-                                            </h6>
-                                            <hr>
-                                            <?php echo $plan['description']; ?>
+                            <div class="card <?php echo $key === 1 ? 'color' : ''; ?>">
+                                <div class="card-body">
+                                    <?php if ( isset( $plan['badge'] ) ) : ?>
+                                        <div class="badge">
+                                            <?php echo $plan['badge']; ?>
                                         </div>
-                                        <div class="d-grid">
-                                            <a href="#" class="btn btn-default text-uppercase" data-course-plan="<?php echo esc_attr( $plan['name'] ); ?>" data-course-id="<?php echo esc_attr( $course_id ); ?>">
-                                                <?php echo $plan['text_button'] ?: esc_html__('Get plan', 'masterstudy-child'); ?>
-                                            </a>
+                                    <?php endif; ?>
+                                    <?php if ( isset( $plan['image'] ) ) : ?>
+                                        <div class="card-image">
+                                            <img src="<?php echo esc_url( $plan['image'] ); ?>" alt="<?php echo esc_attr( $plan['name'] ); ?>">
                                         </div>
+                                    <?php endif; ?>
+                                    <div class="card-header">
+                                        <h5 class="card-title text-uppercase text-center">
+                                            <?php echo $plan['name']; ?>
+                                        </h5>
+                                        <h6 class="card-price text-center">
+                                            <?php echo STM_LMS_Helpers::display_price( $price ); ?>
+                                        </h6>
+                                    </div>
+                                    <div class="card-content">
+                                        <?php echo $plan['description']; ?>
+                                    </div>
+                                    <div class="d-grid">
+                                        <a href="#" class="btn <?php echo $key === 1 ? 'border' : ''; ?> btn-default text-uppercase" data-course-plan="<?php echo esc_attr( $plan['name'] ); ?>" data-course-id="<?php echo esc_attr( $course_id ); ?>">
+                                            <?php echo $plan['text_button'] ?: esc_html__('Get plan', 'masterstudy-child'); ?>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
