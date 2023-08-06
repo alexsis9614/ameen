9b39063a6bd12aeddbfc3ece30a9397946098793
Алексей <6763410@gmail.com>
2023-08-07 00:52:58 +0500

    Expiration update func

--- inc/classes/STM_Cart.php
+++ inc/classes/STM_Cart.php
@@ -2,10 +2,14 @@
     namespace LMS\inc\classes;
 
     use STM_LMS_Cart;
+    use STM_LMS_Helpers;
     use STM_LMS_Woocommerce;
     use WC_Order;
     use STM_LMS_Curriculum;
     use STM_LMS_Options;
+    use STM_LMS_User;
+    use STM_LMS_Order;
+    use STM_LMS_Course;
 
     class STM_Cart extends STM_LMS_Cart
     {
@@ -38,11 +42,13 @@
 
             foreach ($this->statuses as $items) {
                 foreach ( $items as $status => $callback ) {
-                    add_action( $this->prefix . '_order_status' . $status, $callback );
+                    add_action( $this->prefix . '_order_status_' . $status, $callback, 30 );
                 }
             }
 
             add_action( $this->prefix . '_checkout_update_order_meta', array( $this, 'before_create_order' ), 200, 1 );
+
+            add_action( 'stm_lms_order_accepted', array( $this, 'order_accepted' ), 20, 2 );
         }
 
         public static function get_plans( $order_id )
@@ -93,6 +99,30 @@
             }
         }
 
+        public function order_accepted( $user_id, $cart_items )
+        {
+            $accept_order = apply_filters( 'stm_lms_accept_order', true );
+            error_log( print_r( $accept_order, true ) );
+
+            if ( $accept_order ) {
+
+                error_log( print_r( $cart_items, true ) );
+
+                foreach ( $cart_items as $cart_item ) {
+
+                    $user_course = STM_LMS_Helpers::simplify_db_array( stm_lms_get_user_course( $user_id, $cart_item['item_id'] ) );
+                    $end_time    = STM_Course::get_end_time( $cart_item['item_id'], $user_id );
+
+                    error_log( print_r( $user_course, true ) );
+                    error_log( print_r( $end_time, true ) );
+
+                    if ( ! empty( $user_course ) && ! empty( $end_time ) ) {
+                        stm_lms_update_user_course_endtime( $user_course['user_course_id'], $end_time );
+                    }
+                }
+            }
+        }
+
         public function order_created( $order_id )
         {
             $order   = new WC_Order( $order_id );
@@ -105,7 +135,7 @@
                 }
 
                 do_action( 'stm_lms_woocommerce_order_approved', $course, $user_id );
-                do_action( 'stm_lms_woocommerce_send_message_approved', $course, $user_id, $order_id );
+//                do_action( 'stm_lms_woocommerce_send_message_approved', $course, $user_id, $order_id );
             }
         }
 
@@ -128,6 +158,22 @@
                     WC()->cart->remove_cart_item( $cart_item_key );
                 }
             }
+        }
+
+        public static function add_to_cart() {
+            check_ajax_referer( 'stm_lms_add_to_cart', 'nonce' );
+
+            if ( ! is_user_logged_in() || empty( $_GET['item_id'] ) ) {
+                die;
+            }
+
+            $item_id = intval( $_GET['item_id'] );
+            $user    = STM_LMS_User::get_current_user();
+            $user_id = $user['id'];
+
+            $r = self::_add_to_cart( $item_id, $user_id );
+
+            wp_send_json( $r );
         }
 
         public static function _add_to_cart( $item_id, $user_id ) {
