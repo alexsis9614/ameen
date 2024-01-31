90639c0f4ea0fecf375c7b049372f0b546daff12
Алексей <6763410@gmail.com>
2024-02-01 00:10:14 +0500

    Added output data for slider courses

--- stm-lms-templates/shortcodes/stm_lms_ameen_course_bundle.php
+++ stm-lms-templates/shortcodes/stm_lms_ameen_course_bundle.php
@@ -9,10 +9,24 @@
 
     if ( ! empty( $course ) && $course->ID ) :
         $_courses = STM_Course::get_course_bundle( $course->ID );
+
+        if ( empty( $_courses ) ) {
+            return;
+        }
 ?>
 <div class="stm_lms_ameen_course_bundle">
     <div class="stm_lms_ameen_course_bundle__slides">
-        <?php error_log( print_r( $_courses, true ) ); ?>
+        <div class="stm_lms_ameen_course_bundle__slider">
+            <div class="swiper-wrapper">
+                <?php foreach ( $_courses as $_course ) : ?>
+                    <div class="swiper-slide">
+                        <a href="<?php echo esc_url( get_the_permalink( $_course ) ); ?>" class="stm_lms_ameen_course_bundle__item">
+                            <img src="<?php echo esc_url( get_the_post_thumbnail_url( $_course ) ); ?>" alt="<?php echo esc_attr( get_the_title( $_course ) ) ?>">
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
     <div class="stm_lms_ameen_course_bundle__content">
         <h3><?php echo esc_html( $course->post_title ); ?></h3>
